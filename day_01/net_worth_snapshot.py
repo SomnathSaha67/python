64ed002afc46c3cee9f785a431ec94601c32fb15
SomnathSaha2006 <ss0326736@gmail.com>
2026-09-05 13:42:13 +0530

    Add net worth calc using arithmetic operators

---
+++ day_01/net_worth_snapshot.py
@@ -0,0 +1,4 @@
+cash, investments, debt= 9_00_000, 5_00_000, 1_00_000
+net_worth= cash+investments-debt
+print(f"Net worth: ${net_worth}")
+print(f"Monthly runway without earning further: ${net_worth//12}")
